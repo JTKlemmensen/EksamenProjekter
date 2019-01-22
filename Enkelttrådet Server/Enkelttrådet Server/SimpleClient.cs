b9ef2877b274dc23caa2afc42ec90183fdd6f009
Jonas Klemmensen <jonastklemmensen@gmail.com>
2019-01-22 20:35:07 +0100

    Enkelttrådet server

---
+++ Enkelttrådet Server/Enkelttrådet Server/SimpleClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enkelttrådet_Server
+{
+    public class SimpleClient
+    {
+        public SimpleClient(int port)
+        {
+            try
+            {
+                using (Socket connection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    connection.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+
+                    NetworkStream stream = new NetworkStream(connection);
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        writer.WriteLine("Message 1");
+                        writer.WriteLine("Message 2");
+                        writer.WriteLine("Message 3");
+
+                        writer.WriteLine("CLOSE");
+                        writer.Flush();
+                    }
+                }
+                Log("Connection to server is terminated");
+            }catch(Exception)
+            {
+                Log("Exception");
+            }
+        }
+
+        private void Log(string log)
+        {
+            Console.WriteLine("[Client]: "+log);
+        }
+    }
+}
---
+++ Enkelttrådet Server/Enkelttrådet Server/SimpleServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Enkelttrådet_Server
+{
+    public class SimpleServer
+    {
+        private int port;
+        public SimpleServer(int port)
+        {
+            this.port = port;
+            Thread t = new Thread(Run);
+            t.Start();
+        }
+
+        private void Run()
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+
+                using (Socket connection = server.AcceptSocket())
+                {
+                    Log("Client has connected!");
+
+                    NetworkStream stream = new NetworkStream(connection);
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string line = "";
+                        while ((line = reader.ReadLine()) != "CLOSE")
+                            if(line!=null)
+                                Log(line);
+                    }
+                }
+                Log("Server is closed!");
+            }
+            catch (Exception)
+            {
+                Log("Exception");
+            }
+        }
+
+        private void Log(string log)
+        {
+            Console.WriteLine("[Server]: " + log);
+        }
+    }
+}
